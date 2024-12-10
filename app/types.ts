d56519d9272d6a0e5d8a33d8041932deb1dfd27c
Vincent Bianciotto <vincent@celavi.fr>
2024-12-10 22:51:48 +0100

    Avoid keys repetition

--- app/types.ts
+++ app/types.ts
@@ -1,35 +1,34 @@
 import type { FieldError, UseFormRegister } from "react-hook-form";
-import { type ZodType, z } from "zod";
+import { z } from "zod";
 
+// Define the structure of a Pokemon object
 export type Pokemon = {
   name: string;
   image: string;
   types: string[];
 };
 
+// Define the form data structure
 export type FormData = {
   title: string;
   email: string;
   description: string;
 };
 
+// Define valid field names as a union type
+export type ValidFieldNames = keyof FormData;
+
+// Define the props for a form field component
 export type FormFieldProps = {
   type: string;
   placeholder: string;
   name: ValidFieldNames;
   register: UseFormRegister<FormData>;
-  error: FieldError | undefined;
+  error?: FieldError;
 };
 
-export type ValidFieldNames =
-  | "title"
-  | "email"
-  | "description";
-
-
- export const UserSchema: ZodType<FormData> = z
-  .object({
-    title: z.string().min(1, { message: "Title is required" }),
-    email: z.string().email({ message: "Invalid email address" }),
-    description: z.string().min(1, { message: "Description is required" }),
-  });
+export const UserSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  description: z.string().min(1, { message: "Description is required" }),
+});
